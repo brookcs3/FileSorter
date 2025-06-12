ee541fcc0cdc1451e2673ac0816375cc7edaf9ab
Cameron Brooks <brooksc3@oregonstate.edu>
2025-06-11 23:21:37 -0700

    Initial Commit

---
+++ FileSorter/FileSorterApp.swift
@@ -0,0 +1,21 @@
+//
+//  FileSorterApp.swift
+//  FileSorter
+//
+//  Created by Cameron Brooks on 6/11/25.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct FileSorterApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
